964d85acee04af184c9f36d723569cdf7ebc91c1
jrsmith12138 <849070287@qq.com>
2020-12-15 15:36:21 +0800

    fix

--- src/main/java/cn/edu/xmu/goods/service/PresaleServiceInterface.java
+++ src/main/java/cn/edu/xmu/goods/service/PresaleServiceInterface.java
@@ -2,10 +2,10 @@ package cn.edu.xmu.goods.service;
 
 public interface PresaleServiceInterface {
     //根据商品skuId扣除预售商品库存
-    boolean deductPresaleInventory(Long skuId, Integer amount);
+    Boolean deductPresaleInventory(Long skuId, Integer amount);
 
     // 根据商品skuId增加预售商品库存
-    boolean increasePresaleInventory(Long skuId, Integer amount);
+    Boolean increasePresaleInventory(Long skuId, Integer amount);
 
     // 根据（预售）商品skuId获得预售商品定金
     Long getEarnestBySkuId(Long skuId);
